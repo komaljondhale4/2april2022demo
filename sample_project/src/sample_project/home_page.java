8c1a0af0e7e771082ee883df76c13c675d0ce21d
komaljondhale4 <komaljondhale6@gmail.com>
2022-09-21 21:07:35 +0530

    product_page_commit
    I have competed my changes from home page and product page plz check.

--- sample_project/src/sample_project/home_page.java
+++ sample_project/src/sample_project/home_page.java
@@ -1,10 +1,10 @@
 package sample_project;
 
-public class code1 {
+public class home_page {
 
 	public static void main(String[] args) {
 
-		System.out.println("home page");
+		System.out.println(" This is home page");
 		
 	}
 
